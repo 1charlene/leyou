d4c7bf3976f499bc694ee93133d89d25b451262f
chensiyu <962300920@qq.com>
2020-03-26 13:51:24 +0800

    1

--- leyou-upload/src/main/java/com/leyou/LeyouUploadApplication.java
+++ leyou-upload/src/main/java/com/leyou/LeyouUploadApplication.java
@@ -3,13 +3,12 @@ package com.leyou;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@MapperScan("com.leyou.item.mapper")
-public class LeyouItemApplication {
+public class LeyouUploadApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(LeyouItemApplication.class);
+        SpringApplication.run(LeyouUploadApplication.class, args);
     }
 }
